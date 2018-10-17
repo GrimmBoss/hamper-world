f15940883efa9ac7d6e994cf3d39a68e36182daa
GrimmBoss <43271441+GrimmBoss@users.noreply.github.com>
2018-10-17 09:12:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ app = Flask('app')
 @app.route('/', methods=['GET', 'POST'])
 
 def index():
-    return render_template('home.html')
+    return render_template('index.php')
     
 app.run(host='0.0.0.0', port=8080)
